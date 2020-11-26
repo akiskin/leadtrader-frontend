8bfeecb2a4bc17fb8a06687a7a0f8ea8def309c2
Alexander Kiskin <akiskin@luminsports.com>
2020-11-26 11:42:16 +1030

    Playing with router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    sidebar: () => <div>Additional links and info</div>,
+    main: () => <h2>Home</h2>,
+  },
+  {
+    path: "/dashboard",
+    //sidebar: () => <div>dashboard!</div>,
+    main: lazy(() => import("./pages/Dashboard")),
+  },
+];
+
+const App = () => (
+  <Router>
+    <div>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          {routes.map((route, index) =>
+            "sidebar" in route ? (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                children={<route.sidebar />}
+              />
+            ) : null
+          )}
+        </Switch>
+      </div>
+
+      <div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                children={<route.main />}
+              />
+            ))}
+          </Switch>
+        </Suspense>
+      </div>
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,5 @@
+const Dashboard = () => (
+  <div className="text-8xl animate-pulse">Dashboard for us all</div>
+);
+
+export default Dashboard;
