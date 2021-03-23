809de81797b1166b90138068be987188562eafc8
Alexander Kiskin <estel4ever@gmail.com>
2021-03-23 19:29:25 +1030

    SellC list status filter

--- src/pages/sell/SellCampaigns.js
+++ src/pages/sell/SellCampaigns.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { format } from "date-fns";
 
@@ -7,39 +7,63 @@ import { getSellCampaigns } from "store/sellcampaigns/actions";
 import LoadingSpinner from "common/components/LoadingSpinner";
 import { readableStatus } from "common/consts/sellCampaigns";
 
-const SellCampaigns = () => (
-  <>
-    <div className="flex flex-row">
-      <div className="flex-1 mt-5 ml-6 mr-3">
-        <div className="rounded bg-white px-3 py-1">
-          <div className="flex flex-row items-center my-1 h-12">
-            <Link to="/sell/new">
-              <button className="bg-purple-500 text-white border rounded px-2 py-2">
-                Start Selling
-              </button>
-            </Link>
-            <span className="ml-3">Create new sell campaign</span>
+const SellCampaigns = () => {
+  const [showActiveOny, setShowActiveOnly] = useState(false);
+
+  const enabledFilterClasses =
+    "underline text-blue-500 font-bold cursor-pointer";
+  const disabledFilterClasses =
+    "underline text-gray-400 font-light cursor-pointer";
+
+  return (
+    <>
+      <div className="flex flex-row">
+        <div className="flex-1 mt-5 ml-6 mr-3">
+          <div className="rounded bg-white px-3 py-1">
+            <div className="flex flex-row items-center my-1 h-12">
+              <Link to="/sell/new">
+                <button className="bg-purple-500 text-white border rounded px-2 py-2">
+                  Start Selling
+                </button>
+              </Link>
+              <span className="ml-3">Create new sell campaign</span>
+            </div>
           </div>
         </div>
+        <div className="flex-1 mt-5 ml-3 mr-6"></div>
       </div>
-      <div className="flex-1 mt-5 ml-3 mr-6"></div>
-    </div>
 
-    <div className="rounded bg-white py-1 mx-6 mt-6">
-      <div className="mt-5 mx-6 font-semibold text-2xl">Sell Campaigns</div>
-      <div className="mx-6 text-gray-400">
-        Showing{" "}
-        <span className="underline text-blue-500 cursor-pointer">all</span>{" "}
-        campaigns. Click on a campaign to see details.
+      <div className="rounded bg-white py-1 mx-6 mt-6">
+        <div className="mt-5 mx-6 font-semibold text-2xl">Sell Campaigns</div>
+        <div className="mx-6 text-gray-400">
+          Showing{" "}
+          <span
+            className={
+              showActiveOny ? disabledFilterClasses : enabledFilterClasses
+            }
+            onClick={() => setShowActiveOnly(!showActiveOny)}
+          >
+            all
+          </span>{" "}
+          <span
+            className={
+              !showActiveOny ? disabledFilterClasses : enabledFilterClasses
+            }
+            onClick={() => setShowActiveOnly(!showActiveOny)}
+          >
+            active
+          </span>{" "}
+          campaigns. Click on a campaign to see details.
+        </div>
+        <div className="my-5">
+          <SellCampaignList activeOnly={showActiveOny} />
+        </div>
       </div>
-      <div className="my-5">
-        <SellCampaignList />
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
-const SellCampaignList = () => {
+const SellCampaignList = (props) => {
   const dispatch = useDispatch();
   const history = useHistory();
 
@@ -65,21 +89,23 @@ const SellCampaignList = () => {
         </tr>
       </thead>
       <tbody>
-        {list.map((campaign) => (
-          <tr
-            className="border-t border-gray-100 cursor-pointer hover:bg-gray-100"
-            key={campaign.id}
-            onClick={() => history.push(`/sell/${campaign.id}`)}
-          >
-            <td className="pl-6 py-3">{format(campaign.date, "d MMM yy")}</td>
-            <td>{readableStatus(campaign.status)}</td>
-            <td>{campaign.product ? campaign.product.name : null}</td>
-            <td>{campaign.leads_total}</td>
-            <td>{campaign.leads_sold}</td>
-            <td>{campaign.leads_rejected}</td>
-            <td>{campaign.earned}</td>
-          </tr>
-        ))}
+        {list
+          .filter((campaign) => !props.activeOnly || campaign.status === 10)
+          .map((campaign) => (
+            <tr
+              className="border-t border-gray-100 cursor-pointer hover:bg-gray-100"
+              key={campaign.id}
+              onClick={() => history.push(`/sell/${campaign.id}`)}
+            >
+              <td className="pl-6 py-3">{format(campaign.date, "d MMM yy")}</td>
+              <td>{readableStatus(campaign.status)}</td>
+              <td>{campaign.product ? campaign.product.name : null}</td>
+              <td>{campaign.leads_total}</td>
+              <td>{campaign.leads_sold}</td>
+              <td>{campaign.leads_rejected}</td>
+              <td>{campaign.earned}</td>
+            </tr>
+          ))}
       </tbody>
     </table>
   );
